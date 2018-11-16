15c6d7e340a2aa4d7be4da9939dd960b96f2d419
dom <34490428+qu-ota@users.noreply.github.com>
2018-11-16 14:15:16 -0500

    Update server.cs

--- server.cs
+++ server.cs
@@ -1,4 +1,17 @@
-//all modules will be loaded here, for now nothing will be here
+//all modules will be loaded here. if adding new modules, make sure you put in a line here.
+//example: exec("./<MODULE>.cs");
+//capitalization not required, do not include <> when adding line
+
+$afkNameChanges = 1;
 exec("*/back.cs");
 exec("*/afk.cs");
 exec("*/brb.cs");
+
+functon serverCmdToggleAfkNameChanges(%client)
+{
+	if(%client.isSuperAdmin)
+	{
+		if($afkNameChanges == 1)
+		{
+			$afkNameChanges = 0;
+			//to be continued soon
