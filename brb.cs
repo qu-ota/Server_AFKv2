aa6f19730a801233e0078f0440bcecf1564a7f11
dom <34490428+qu-ota@users.noreply.github.com>
2018-11-16 14:11:26 -0500

    Add AFK name changes pref

--- brb.cs
+++ brb.cs
@@ -27,7 +27,10 @@ function serverCmdBrb(%client, %c1, %c2, %c3, %c4, %c5, %c6, %c7, %c8, %c9, %c10
 				%client.status = "afk";
 				%client.canStatus = 0;
 				schedule(5000, 0, canstatusnow, %client);
-				%client.player.setShapeName(%client.name SPC "(AFK)", 8564862);
+				if($afkNameChanges = 1)
+				{
+					%client.player.setShapeName(%client.name SPC "(AFK)", 8564862);
+				}
 				
 			}
 			else
@@ -37,7 +40,10 @@ function serverCmdBrb(%client, %c1, %c2, %c3, %c4, %c5, %c6, %c7, %c8, %c9, %c10
 				%client.status = "afk";
 				%client.canStatus = 0;
 				schedule(5000, 0, canstatusnow, %client);
-				%client.player.setShapeName(%client.name SPC "(AFK)", 8564862);
+				if($afkNameChanges = 1)
+				{
+					%client.player.setShapeName(%client.name SPC "(AFK)", 8564862);
+				}
 			}
 		}
 		else
