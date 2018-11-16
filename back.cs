6f15c52e148ee16c632bf1660fc090db8dbdedb5
dom <34490428+qu-ota@users.noreply.github.com>
2018-11-16 08:21:47 -0500

    Create back.cs

---
+++ back.cs
@@ -0,0 +1,19 @@
+//Back command .cs file
+//Marks user as "not AFK"
+//Loaded from server.cs
+//Make changes to the messages or script here to change only the /back command
+
+function serverCmdBack(%client)
+{
+	if(%client.status $= "here")
+	{
+		messageClient(%client,'',"<color:F660AB>You aren't AFK.");
+	}
+	else
+	{
+		messageAll('',"\c3" @ %client.name @ "<color:C8B560> is back!");
+		%client.reason = "";
+		%client.status = "here";
+		%client.player.setShapeName(%client.name, 8564862);
+	}
+}
