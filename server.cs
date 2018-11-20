8a73783851573efe27c36868dbad1186736d474b
dom <34490428+qu-ota@users.noreply.github.com>
2018-11-20 08:17:27 -0500

    Remove prefs so they can be moved to prefs.cs

--- server.cs
+++ server.cs
@@ -16,46 +16,22 @@ if(isFile("./brb.cs"))
 //now for the extra functions
 
 function canStatusNow(%client)
-	{
-		%client.canStatus = 1;
-	}
+{
+	%client.canStatus = 1;
+}
 
-	function announceafkloop()
+function announceafkloop()
+{
+	if($StatusMod::AnnounceAFK == 1)
 	{
-		if($StatusMod::AnnounceAFK == 1)
+		for(%i = 0; %i < ClientGroup.getCount(); %i++)
 		{
-			for(%i = 0; %i < ClientGroup.getCount(); %i++)
+			%cl = ClientGroup.getObject(%i);
+			if(%cl.status $= "afk")
 			{
-				%cl = ClientGroup.getObject(%i);
-				if(%cl.status $= "afk")
-				{
-					messageall('',"\c3" @ %cl.name @ "\c6 Is AFK: \c4" @ %cl.reason);
-				}
-			}        
-		}
-		schedule($StatusMod::LoopTime * 1000, 0, announceafkloop);
+				messageall('',"\c3" @ %cl.name @ "\c6 Is AFK: \c4" @ %cl.reason);
+			}
+		}        
 	}
-
-if(isFile("Add-Ons/System_ReturnToBlockland/server.cs") || isFile("Add-Ons/System_BlocklandGlass/server.cs"))
-{
-	if(!$RTB::RTBR_ServerControl_Hook)
-	{
-		RTB_registerPref("Announce AFK","Status Mod v2","StatusModv2::AnnounceAFK","bool","Server_StatusModv2",1,0,0);
-		RTB_registerPref("Announce Time (Seconds)","Status Mod v2","StatusModv2::LoopTime","int 60 1200","StatusModv2",300,0,0);
-	}
+	schedule($StatusMod::LoopTime * 1000, 0, announceafkloop);
 }
-else
-{
-		//Change these prefs if you don't have Return to Blockland.
-	$StatusMod::AnnounceAFK = 1;
-	$StatusMod::LoopTime = 300;
-}
-
-functon serverCmdToggleAfkNameChanges(%client)
-{
-	if(%client.isSuperAdmin)
-	{
-		if($afkNameChanges == 1)
-		{
-			$afkNameChanges = 0;
-			//to be continued soon
